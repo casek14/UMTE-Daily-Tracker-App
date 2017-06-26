c10a4228c1c7f8be253ba4ad6e9787557fd9905c
Jan Cach <cach.jan@gmail.com>
2017-06-26 22:17:17 +0200

    Add subcategory

---
+++ app/src/main/java/com/example/casek/beta01/add_subcategory.java
@@ -0,0 +1,13 @@
+package com.example.casek.beta01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_subcategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_subcategory);
+    }
+}
